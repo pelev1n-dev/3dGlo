083a0bb56a8b093b3ac2719ac02f5347a81e0584
Pelevin Aleksandr <web.it.seb@gmail.com>
2020-02-21 22:11:57 +0300

    lesson_18 hard complete

--- script.js
+++ script.js
@@ -104,4 +104,19 @@ window.addEventListener('DOMContentLoaded', function () {
   };
   togglePopUp();
 
+  //Scroll
+  const anchors = document.querySelectorAll('a[href*="#"]');
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const blockID = anchor.getAttribute('href').substring(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
 });
