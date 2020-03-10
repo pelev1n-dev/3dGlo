8603e71c9da1777d2579eaf38186d59bc9ee8eba
Pelevin Aleksandr <web.it.seb@gmail.com>
2020-03-10 10:30:48 +0300

    refact: fix validate

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -2,21 +2,40 @@ const sendForm = () => {
   const errorMessage = 'Что то пошло не так...';
   const loadMessage = document.createElement('div');
   const successMessage = 'Спасибо! Мы с Вами свяжемся.';
-  const form = document.getElementById('form1');
+  const form1 = document.getElementById('form1');
+  const form2 = document.getElementById('form2');
+  const form3 = document.getElementById('form3');
   const statusMessage = document.createElement('div');
   statusMessage.style.cssText = 'font-size: 2rem;';
 
-  form.addEventListener('submit', (event) => {
+  const inputName = document.querySelectorAll('input[type = text]');
+  const inputMessage = document.querySelector('.mess');
+
+  inputName[0].addEventListener('input', () => {
+    inputName[0].value = inputName[0].value.replace(/[^,А-Яа-яЁё\s]/, '');
+  });
+  inputName[1].addEventListener('input', () => {
+    inputName[1].value = inputName[1].value.replace(/[^,А-Яа-яЁё\s]/, '');
+  });
+  inputName[2].addEventListener('input', () => {
+    inputName[2].value = inputName[2].value.replace(/[^,А-Яа-яЁё\s]/, '');
+  });
+
+  inputMessage.addEventListener('input', () => {
+    inputMessage.value = inputMessage.value.replace(/[^,А-Яа-яЁё\s]/, '');
+  });
+
+  form1.addEventListener('submit', (event) => {
     event.preventDefault();
-    form.appendChild(loadMessage);
-    form.appendChild(statusMessage);
+    form1.appendChild(loadMessage);
+    form1.appendChild(statusMessage);
     loadMessage.textContent = loadMessage.classList.add('spinning-square');
-    const formData = new FormData(form);
-    let body = {};
+    const formData = new FormData(form1);
+    let body1 = {};
     formData.forEach((val, key) => {
-      body[key] = val;
+      body1[key] = val;
     });
-    postData(body);
+    postData(body1);
   });
 
   const postData = () => {
@@ -24,17 +43,32 @@ const sendForm = () => {
         .then((response) => {
           if (response.status !== 200){
             statusMessage.textContent = errorMessage;
-            form.removeChild(loadMessage);
+            form1.removeChild(loadMessage);
             throw new Error('status network not 200.');
           }
           return(response.text());
         })
         .then((data) => {
           statusMessage.textContent = successMessage;
-          form.removeChild(loadMessage);
-          form.reset();
+          form1.removeChild(loadMessage);
+          form1.reset();
+          setTimeout(() => {
+            form1.removeChild(statusMessage);
+          }, 5000);
         })
         .catch((error) => console.log(error));
-  }
+  };
+
+  form2.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form1.appendChild(loadMessage);
+    form1.appendChild(statusMessage);
+    const formData = new FormData(form2);
+    let body = {};
+    formData.forEach((val, key) => {
+      body[key] = val;
+    });
+    postData(body);
+  });
 };
 export default sendForm;
